e9d9e342b288e87db6c1b25f1a42a934c85cd680
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-07-29 17:57:28 -0700

    Pull in bootstrap resources. Create bootstrap template in inex.html. Create main controller and main partial for angular.

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -4,5 +4,6 @@
 // Declare app level module which depends on filters, and services
 angular.module('todo', ['todo.controllers']).
   config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'mainCtrl'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
