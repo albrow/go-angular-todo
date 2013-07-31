5ab7f6c69ff6c724aa8a0261da242ea28321dd20
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-07-30 19:20:31 -0700

    Add transition to new items

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -5,6 +5,7 @@
 angular.module('todo.controllers', []).
 	controller('mainCtrl', ['$scope', '$http', 'Restangular', function($scope, $http, Restangular) {
 		delete $http.defaults.headers.common['X-Requested-With'];
+		$scope.currentNewItem = null;
 
 		var baseItems = Restangular.all('items');
 		baseItems.getList().then(function(items) {
@@ -14,8 +15,13 @@ angular.module('todo.controllers', []).
 		$scope.addItem = function(item) {
 			if (item) {
 				baseItems.post(item).then(function(item) {
-					$scope.items.push(item);
-					$("#item-content").val("");
+					$scope.currentNewItem = item;
+					$scope.items.unshift(item);
+					$("#item-content-input").val("");
+					setTimeout(function() {
+						$scope.currentNewItem = null;
+						$scope.$digest();
+					}, 10);
 				});
 			}
 		}
